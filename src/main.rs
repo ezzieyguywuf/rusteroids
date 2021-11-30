7638aa9e4447cc9a68afaf1b919e2b964f3bf7bc
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-11-30 12:46:56 -0500

    Basic crossterm raw mode example
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stdout};
+use crossterm as xtrm;
+
+fn main() -> xtrm::Result<()> {
+    println!("Hello, world! (top)");
+    let (cols, rows) = xtrm::terminal::size()?;
+    // Resize terminal and scrollup
+    xtrm::execute!(
+        stdout(),
+        xtrm::terminal::SetSize(10, 10),
+        xtrm::terminal::ScrollUp(5)
+    )?;
+
+    // Be a good citizen and clean up
+    xtrm::execute!(stdout(), xtrm::terminal::SetSize(cols, rows))?;
+    println!("Hello, world! (bottom)");
+    Ok(())
 }
