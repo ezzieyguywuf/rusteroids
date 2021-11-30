b0c11bda5a6db0bdb6a6524abe80f8b1fbc8aba4
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-11-30 15:48:46 -0500

    Add basic cursor functionality
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,33 +11,51 @@ fn main() -> xtrm::Result<()> {
 
     // Queue up some things to do
     let (cols, rows) = xtrm::terminal::size()?;
+    let mut x = 0;
+    let mut y = 0;
     stdout.queue(xtrm::terminal::EnterAlternateScreen)?
+          .queue(xtrm::cursor::Hide)?
           .queue(xtrm::style::Print("before resizing".to_string()))?
-          .queue(xtrm::terminal::SetSize(10, 10))?
+          .queue(xtrm::terminal::SetSize(1000, 1000))?
           .queue(xtrm::style::Print("after resizing".to_string()))?
-          .queue(xtrm::cursor::MoveTo(0, 0))?
+          .queue(xtrm::cursor::MoveTo(0, 1))?
           .queue(xtrm::style::Print("After MoveTo".to_string()))?;
 
     loop {
-        if xtrm::event::poll(time::Duration::from_millis(500))? {
+        if xtrm::event::poll(time::Duration::from_millis(1))? {
             match xtrm::event::read()? {
                 xtrm::event::Event::Key(event) => {
                     if event.code == xtrm::event::KeyCode::Char('q') {
                         break;
+                    } else if event.code == xtrm::event::KeyCode::Up {
+                        stdout.queue(xtrm::cursor::MoveTo(x, y))?.queue(xtrm::style::Print(" "))?;
+                        y -= 1;
+                    } else if event.code == xtrm::event::KeyCode::Down {
+                        stdout.queue(xtrm::cursor::MoveTo(x, y))?.queue(xtrm::style::Print(" "))?;
+                        y += 1;
+                    } else if event.code == xtrm::event::KeyCode::Left {
+                        stdout.queue(xtrm::cursor::MoveTo(x, y))?.queue(xtrm::style::Print(" "))?;
+                        x -= 1;
+                    } else if event.code == xtrm::event::KeyCode::Right {
+                        stdout.queue(xtrm::cursor::MoveTo(x, y))?.queue(xtrm::style::Print(" "))?;
+                        x += 1;
                     }
                 },
                 xtrm::event::Event::Mouse(_) => (),
                 xtrm::event::Event::Resize(_, _) => (),
             }
         } else {
-            stdout.queue(xtrm::cursor::MoveTo(0, 0))?
+            stdout.queue(xtrm::cursor::MoveTo(0, 2))?
                   .queue(xtrm::style::Print("...waiting\n"))?;
-            stdout.flush()?;
-            std::thread::sleep(time::Duration::from_millis(500));
         }
+        stdout.queue(xtrm::cursor::MoveTo(x, y))?
+              .queue(xtrm::style::Print("."))?;
+        stdout.flush()?;
+        std::thread::sleep(time::Duration::from_millis(1));
     };
 
     stdout.queue(xtrm::terminal::SetSize(cols, rows))?
+          .queue(xtrm::cursor::Show)?
           .queue(xtrm::terminal::LeaveAlternateScreen)?;
 
     // disable raw mode
