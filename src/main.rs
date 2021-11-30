896f6d2a59a2d5a8b36b4b5a8d720e36c12726ef
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2021-11-30 14:11:19 -0500

    Use alternate term screen
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,30 @@ use std::io::{stdout};
 use crossterm as xtrm;
 
 fn main() -> xtrm::Result<()> {
+    let mut stdout = stdout();
+
     // enable raw mode
+    println!("Hello, world! (top)");
     xtrm::terminal::enable_raw_mode()?;
 
-    println!("Hello, world! (top)");
-    println!("rawModeEnabled = {}", xtrm::terminal::is_raw_mode_enabled()?);
+    // Queue up some things to do
     let (cols, rows) = xtrm::terminal::size()?;
-    // Resize terminal and scrollup
-    xtrm::execute!(
-        stdout(),
+    xtrm::queue!(
+        stdout,
+        xtrm::terminal::EnterAlternateScreen,
+        xtrm::style::Print("before resizing".to_string()),
         xtrm::terminal::SetSize(10, 10),
-        xtrm::terminal::ScrollUp(5)
+        xtrm::style::Print("after resizing".to_string()),
+        xtrm::cursor::MoveTo(0, 0),
+        xtrm::style::Print("After MoveTo".to_string()),
+        // xtrm::terminal::ScrollDown(5),
+        // xtrm::style::Print("<-- final location".to_string()),
+        xtrm::terminal::SetSize(cols, rows), // reset terminal
+        xtrm::terminal::LeaveAlternateScreen,
     )?;
-
-    // Be a good citizen and clean up
-    xtrm::execute!(stdout(), xtrm::terminal::SetSize(cols, rows))?;
-    println!("Hello, world! (bottom)");
 
     // disable raw mode
     xtrm::terminal::disable_raw_mode()?;
-    println!("rawModeEnabled = {}", xtrm::terminal::is_raw_mode_enabled()?);
+    println!("Hello, world! (bottom)");
     Ok(())
 }
