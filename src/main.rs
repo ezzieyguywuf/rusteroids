34eeb88a8f9ec5da66d228e2d2dbd3014905f561
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-11-30 23:27:55 -0500

    Formatting
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use glfw;
 use std::{io, io::Write, cell};
 
 fn error_callback(err: glfw::Error, desc: String, error_count: &cell::Cell<usize>) {
-    io::stderr().write_all(format!("GLFW error {} - {}: {}\n", error_count.get(), err, desc).to_string().as_bytes()).unwrap();
+    io::stderr().write_all(
+        format!("GLFW error {} - {}: {}\n", error_count.get(), err, desc)
+        .to_string().as_bytes()).unwrap();
     error_count.set(error_count.get() + 1);
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let err = Some(glfw::Callback{
         f: error_callback as fn(glfw::Error, String, &cell::Cell<usize>),
         data: cell::Cell::new(0)
@@ -17,4 +19,6 @@ fn main() {
     let _ = glfw_.create_window(300, 300, "Hey this won't work.", glfw::WindowMode::Windowed);
     let _ = glfw_.create_window(300, 300, "Nope, not working.", glfw::WindowMode::Windowed);
     let _ = glfw_.create_window(300, 300, "stop it! :(", glfw::WindowMode::Windowed);
+
+    Ok(())
 }
