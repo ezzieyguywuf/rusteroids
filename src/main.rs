0c85075271f5adbe25d7db11645d6ff74f0cafcb
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-11-30 23:40:59 -0500

    glfw: basic black window with input handling
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use glfw;
+use glfw::{self, Context};
 use std::{io, io::Write, cell};
 
 fn error_callback(err: glfw::Error, desc: String, error_count: &cell::Cell<usize>) {
@@ -15,10 +15,30 @@ fn main() -> io::Result<()> {
     });
     let mut glfw_ = glfw::init(err).unwrap();
 
-    glfw_.window_hint(glfw::WindowHint::ContextVersion(40000, 3000)); // Ridiculous!
-    let _ = glfw_.create_window(300, 300, "Hey this won't work.", glfw::WindowMode::Windowed);
-    let _ = glfw_.create_window(300, 300, "Nope, not working.", glfw::WindowMode::Windowed);
-    let _ = glfw_.create_window(300, 300, "stop it! :(", glfw::WindowMode::Windowed);
+    // glfw_.window_hint(glfw::WindowHint::ContextVersion(40000, 3000)); // Ridiculous!
+    let (mut window, events) =
+        glfw_
+        .create_window(300, 300, "Hello rusteroids!", glfw::WindowMode::Windowed)
+        .expect("Failed to create glfw window");
+
+    window.make_current();
+    window.set_key_polling(true);
+
+    while !window.should_close() {
+        window.swap_buffers();
+
+        glfw_.poll_events();
+
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("{:?}", event);
+            match event {
+                glfw::WindowEvent::Key(glfw::Key::Escape, _, glfw::Action::Press, _) => {
+                    window.set_should_close(true);
+                },
+                _ => {},
+            }
+        }
+    }
 
     Ok(())
 }
