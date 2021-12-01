3ebbed7b057fb9f2b8a4b09264fdaf8d4901f7c1
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-11-30 23:16:53 -0500

    Basic glfw integration with error callback
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,20 @@
-use std::io::{self, Write};
-use std::{time};
-use crossterm::{self as xtrm, QueueableCommand};
+use glfw;
+use std::{io, io::Write, cell};
 
-fn main() -> xtrm::Result<()> {
-    let mut stdout = io::stdout();
+fn error_callback(err: glfw::Error, desc: String, error_count: &cell::Cell<usize>) {
+    io::stderr().write_all(format!("GLFW error {} - {}: {}\n", error_count.get(), err, desc).to_string().as_bytes()).unwrap();
+    error_count.set(error_count.get() + 1);
+}
 
-    // enable raw mode
-    println!("Hello, world! (top)");
-    xtrm::terminal::enable_raw_mode()?;
+fn main() {
+    let err = Some(glfw::Callback{
+        f: error_callback as fn(glfw::Error, String, &cell::Cell<usize>),
+        data: cell::Cell::new(0)
+    });
+    let mut glfw_ = glfw::init(err).unwrap();
 
-    // Queue up some things to do
-    let (cols, rows) = xtrm::terminal::size()?;
-    let mut x = 0;
-    let mut y = 0;
-    stdout.queue(xtrm::terminal::EnterAlternateScreen)?
-          .queue(xtrm::cursor::Hide)?
-          .queue(xtrm::style::Print("before resizing".to_string()))?
-          .queue(xtrm::terminal::SetSize(10, 10))?
-          .queue(xtrm::style::Print("after resizing".to_string()))?
-          .queue(xtrm::cursor::MoveTo(0, 1))?
-          .queue(xtrm::style::Print("After MoveTo".to_string()))?;
-
-    loop {
-        if xtrm::event::poll(time::Duration::from_millis(1))? {
-            match xtrm::event::read()? {
-                xtrm::event::Event::Key(event) => {
-                    if event.code == xtrm::event::KeyCode::Char('q') {
-                        break;
-                    } else if event.code == xtrm::event::KeyCode::Up {
-                        stdout.queue(xtrm::cursor::MoveTo(x, y))?.queue(xtrm::style::Print(" "))?;
-                        y -= 1;
-                    } else if event.code == xtrm::event::KeyCode::Down {
-                        stdout.queue(xtrm::cursor::MoveTo(x, y))?.queue(xtrm::style::Print(" "))?;
-                        y += 1;
-                    } else if event.code == xtrm::event::KeyCode::Left {
-                        stdout.queue(xtrm::cursor::MoveTo(x, y))?.queue(xtrm::style::Print(" "))?;
-                        x -= 1;
-                    } else if event.code == xtrm::event::KeyCode::Right {
-                        stdout.queue(xtrm::cursor::MoveTo(x, y))?.queue(xtrm::style::Print(" "))?;
-                        x += 1;
-                    }
-                },
-                xtrm::event::Event::Mouse(_) => (),
-                xtrm::event::Event::Resize(_, _) => (),
-            }
-        } else {
-            stdout.queue(xtrm::cursor::MoveTo(0, 2))?
-                  .queue(xtrm::style::Print("...waiting\n"))?;
-        }
-        stdout.queue(xtrm::cursor::MoveTo(0, 0))?
-              .queue(xtrm::style::Print("                              "))?
-              .queue(xtrm::cursor::MoveTo(0, 0))?
-              .queue(xtrm::style::Print(format!("x = {}, y = {}", x, y)))?
-              .queue(xtrm::cursor::MoveTo(x, y))?
-              .queue(xtrm::style::Print("."))?;
-        stdout.flush()?;
-        std::thread::sleep(time::Duration::from_millis(1));
-    };
-
-    stdout.queue(xtrm::terminal::SetSize(cols, rows))?
-          .queue(xtrm::cursor::Show)?
-          .queue(xtrm::terminal::LeaveAlternateScreen)?;
-
-    // disable raw mode
-    xtrm::terminal::disable_raw_mode()?;
-    println!("Hello, world! (bottom)");
-    Ok(())
+    glfw_.window_hint(glfw::WindowHint::ContextVersion(40000, 3000)); // Ridiculous!
+    let _ = glfw_.create_window(300, 300, "Hey this won't work.", glfw::WindowMode::Windowed);
+    let _ = glfw_.create_window(300, 300, "Nope, not working.", glfw::WindowMode::Windowed);
+    let _ = glfw_.create_window(300, 300, "stop it! :(", glfw::WindowMode::Windowed);
 }
